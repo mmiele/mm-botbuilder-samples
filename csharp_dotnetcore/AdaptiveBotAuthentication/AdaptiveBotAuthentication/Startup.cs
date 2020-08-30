a4998786fb7817f0705b35618cc54a201f1843c9
Michael Miele <v-mimiel@microsoft.com>
2020-08-30 12:57:59 -0700

    Compted exercise

--- csharp_dotnetcore/AdaptiveBotAuthentication/AdaptiveBotAuthentication/Startup.cs
+++ csharp_dotnetcore/AdaptiveBotAuthentication/AdaptiveBotAuthentication/Startup.cs
@@ -3,10 +3,15 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EmptyBot v4.9.2
 
+using AdaptiveBotAuthentication.Dialogs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,7 +36,39 @@ namespace AdaptiveBotAuthentication
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EmptyBot>();
+            // services.AddTransient<IBot, DialogBot>();
+            
+            // Previous line replaced by 
+            // Create the bot. the ASP Controller is expecting an IBot.
+            services.AddSingleton<IBot, DialogBot<RootDialog>>();
+
+            /** Adaptive dialogs **/
+
+            // Register dialog. This sets up memory paths for adaptive.
+            ComponentRegistration.Add(new DialogsComponentRegistration());
+
+            // Register adaptive component
+            ComponentRegistration.Add(new AdaptiveComponentRegistration());
+
+            // Register to use language generation.
+            ComponentRegistration.Add(new LanguageGenerationComponentRegistration());
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.) 
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+
+            // The adaptive dialog that will be run by the bot.
+            services.AddSingleton<RootDialog>();
+
+          
 
 
         }
